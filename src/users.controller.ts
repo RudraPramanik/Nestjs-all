cc688de051be9574fa6acbc1904bd76724fe9d74
Rudra <rudrahimangshoo2020@gmail.com>
2024-05-16 16:36:28 +0600

    sending response

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,18 +1,46 @@
-import { Controller, Get, Req, Post, Put, Delete, Patch } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Controller,
+  Get,
+  Req,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Header,
+  Redirect,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { PassThrough } from 'stream';
 
 @Controller('/users')
 export class UsersController {
   @Post('/profile')
-  getProfile(@Req() req: Request) {
-    return 'hello rudra';
+  @Redirect('/users/accounts')
+  @Header('Cache-control', 'none')
+  @Header('X-name', 'Rudra')
+  getProfile() {
+    const rn = Math.random() * 10 + 1;
+    if (rn > 5) {
+      return {
+        url: '/users/accounts',
+        statusCode: 302,
+      };
+    } else {
+      return {
+        url: '/users/wallet',
+        statusCode: 302,
+      };
+    }
   }
-  @Get('/rudra')
-  getRudra() {
-    return 'hi rudra';
+  @Get('/accounts')
+  redirectRoute() {
+    return 'redirected route';
   }
-  @Put('/pramanik')
-  getPramanik(){
-    return 'hi pramanik'
+  @Get('/wallet')
+  redirectWallet(){
+    return 'redicrct wallet'
   }
 }
