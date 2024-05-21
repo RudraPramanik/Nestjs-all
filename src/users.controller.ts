1133754ee282dd7db69a5c09ecb2794991b19854
Rudra <rudrahimangshoo2020@gmail.com>
2024-05-21 13:15:54 +0600

    route-crud

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,33 +1,84 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
-  Req,
+  Param,
   Post,
   Put,
-  Delete,
-  Patch,
-  HttpCode,
-  HttpStatus,
-  Res,
-  Header,
-  Redirect,
-  Body,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { PassThrough } from 'stream';
+import { createUserDTO } from './dto';
 
-interface videoDTO {
-  name: string;
-  tag: string;
-}
+let user = [];
+
 @Controller('/users')
 export class UsersController {
-  @Post('/video')
-  addVideo(@Body() requestData: videoDTO) {
-    console.log(requestData);
-    return { success: true };
+  @Post()
+  addUsers(@Body() createUser: createUserDTO) {
+    user.push(createUser);
+    console.log(user);
+    return 'useradded';
+  }
+  @Get()
+  getUsers() {
+    console.log('get user hitted');
+    return user;
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: number) {
+    return user.find((user) => user.id === +id);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() userUpdateDTO: createUserDTO) {
+    const userIdx = user.findIndex((user) => +user.id === +id);
+
+    if (!userIdx) {
+      return;
+    }
+    user[userIdx] = userUpdateDTO;
+    console.log(user)
+    return 'updated';
+  }
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    user = user.filter((user) => +user.id !== +id);
+    console.log(user)
+    return ' user deleted';
   }
 }
+
+//body7 import {
+//   Controller,
+//   Get,
+//   Req,
+//   Post,
+//   Put,
+//   Delete,
+//   Patch,
+//   HttpCode,
+//   HttpStatus,
+//   Res,
+//   Header,
+//   Redirect,
+//   Body,
+// } from '@nestjs/common';
+// import { Request, Response } from 'express';
+// import { PassThrough } from 'stream';
+
+// interface videoDTO {
+//   name: string;
+//   tag: string;
+// }
+// @Controller('/users')
+// export class UsersController {
+//   @Post('/video')
+//   addVideo(@Body() requestData: videoDTO) {
+//     console.log(requestData);
+//     return { success: true };
+//   }
+// }
 
 // @Post('/profile')
 // @Redirect('/users/accounts')
